f94dd6e48208a6dc057ed036c15bb000c480ee3f
Poga Po <poga.bahamut@gmail.com>
2016-06-27 00:55:54 +0800

    init

---
+++ index.js
@@ -0,0 +1,32 @@
+$(function() {
+    $(".page-link").on("click", function (e) {
+        renderPage($(this).data("page"))
+    })
+
+    $("img.lazy").lazyload({
+        effect : "fadeIn"
+    });
+
+    if (window.location.hash != "" ){
+        renderPage("page-" + window.location.hash.replace("#", ""))
+    } else {
+        renderPage($($(".page-link")[0]).data("page"))
+    }
+
+})
+
+
+function clearActive() {
+    $(".page-link").parent().removeClass("active")
+}
+
+function renderPage(page) {
+    clearActive()
+    $(".page").css("display", "none")
+    $(".page").removeClass("active-page")
+    $("#" + page).css("display", "block")
+    $("#" + page).addClass("active-page")
+    $("#link-" + page).parent().addClass("active")
+    $("img.lazy").lazyload();
+}
+
